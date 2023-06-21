1d8fa4aaf2bfee9336f1dce7fc5e270568b94acb
Julia Brauer <julia.brauer83@gmail.com>
2023-06-20 21:33:45 -0400

    more game logic

--- app.js
+++ app.js
@@ -1,3 +1,86 @@
+
+function randomInt(min, max) { // taken from MDN website; both min and max are inclusive
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min); 
+}
+
+
+
+class Spaceship {
+
+  constructor(hull, firepower, accuracy) {
+    this.hull = hull;
+    this.firepower = firepower;
+    this.accuracy = accuracy;
+  }
+
+  attack(otherShip) {
+    let successKey = Math.random();
+    //console.log(successKey);
+    if (successKey <= this.accuracy) {
+      console.log("HIT");
+      otherShip.hull -= this.firepower;
+    } 
+  }
+
+  isAlive() {
+    return (this.hull > 0);
+  }
+}
+
+
+class Game {
+
+  assembly;
+  alienFleet = [];
+  fleetStrength = 6;
+  roundCount = 0;
+  currentEnemy;
+
+  constructor() {
+
+    this.alienFleet = Array.from(Array(this.fleetStrength), () => {
+      let h = randomInt(3, 6);
+      let fp = randomInt(2, 4);
+      let ac = randomInt(6, 8)/10.0;
+      return new Spaceship(h, fp, ac);
+    });
+
+    this.assembly = new Spaceship(20, 5, .7);
+    this.currentEnemy = this.alienFleet.pop();
+    console.log("NEW ENEMY");
+  }
+
+
+  playRound() {
+    this.roundCount++;
+    this.assembly.attack(this.currentEnemy);
+    this.currentEnemy.attack(this.assembly);
+    console.log("Round " + this.roundCount + ": ")
+    console.log("Assembly: ", this.assembly);
+    console.log("Enemy: ", this.currentEnemy);
+  }
+
+  fightEnemy() {
+    while (this.assembly.isAlive() && this.currentEnemy.isAlive()) {
+      this.playRound();
+    }
+  }
+
+  playGame() {
+    while(this.alienFleet.length > 0) {
+      this.currentEnemy = this.alienFleet.pop();
+      console.log(this.currentEnemy);
+      this.fightEnemy();
+    }
+  }
+}
+
+const myGame = new Game();
+myGame.playGame();
+
+
 /* SPECS
 There are six alien ships. 
 The aliens' weakness is that they are too logical and attack one at a time: ...
@@ -27,62 +110,3 @@ X accuracy is the chance between 0 and 1 that the ship will hit its target
 USS ASSEMBLY: hull: 20; firepower: 5; accuracy: .7
 ALIENS: hull: range(3,6); firepower: range(2,4); accuracy: range(.6,.8)
 */
-
-function getRandom(min, max) { // taken from MDN website
-  return Math.floor(Math.random() * (max - min) + min);
-}
-
-
-class Spaceship {
-  constructor(hull, firepower, accuracy) {
-    this.hull = hull;
-    this.firepower = firepower;
-    this.accuracy = accuracy;
-  }
-
-  attack(otherShip) {
-    let successKey = Math.random();
-    console.log(successKey);
-    if (successKey <= this.accuracy) {
-      otherShip.hull -= this.firepower;
-    } 
-  }
-
-  isAlive() {
-    return (this.hull > 0);
-  }
-}
-
-class Game {
-
-  roundCount = 0;
-  assembly = new Spaceship(20, 5, .7);
-  alien = new Spaceship(3, 2, .6);
-
-  constructor() {
-    console.log(this.assembly);
-    console.log(this.alien);
-  }
-
-  playRound() {
-    this.roundCount++;
-    this.assembly.attack(this.alien);
-    this.alien.attack(this.assembly);
-    console.log("Round " + this.roundCount + ": ")
-    console.log(this.assembly);
-    console.log(this.alien);
-  }
-
-  playGame() {
-    while (this.assembly.isAlive() && this.alien.isAlive()) {
-      this.playRound();
-    }
-  }
-}
-
-const myGame = new Game();
-myGame.playGame();
-
-
-
-
